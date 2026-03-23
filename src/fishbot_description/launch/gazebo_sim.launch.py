c7be02ffe87329eb0daf4a70201092453d018b83
sceneeeee <peterufo060419@gmail.com>
2026-03-23 17:54:55 +0800

    add gazebo sensors and ros2_control support

--- src/fishbot_description/launch/gazebo_sim.launch.py
+++ src/fishbot_description/launch/gazebo_sim.launch.py
@@ -44,9 +44,36 @@ def generate_launch_description():
         output='screen'
     )
 
+    action_load_joint_state_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_joint_state_broadcaster --set-state active' .split(' '),
+        output='screen'
+    )
+
+    # action_load_effort_controller = launch.actions.ExecuteProcess(
+    #     cmd='ros2 control load_controller fishbot_effort_controller --set-state active' .split(' '),
+    #     output='screen'
+    # )
+
+    action_load_diff_drive_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_diff_drive_controller --set-state active' .split(' '),
+        output='screen'
+    )
+
     return launch.LaunchDescription([
         action_declare_arg_mode_path,
         action_robot_state_publisher_node,
         action_launch_gazebo,
-        action_spawn_entity
+        action_spawn_entity,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_spawn_entity,
+                on_exit=[action_load_joint_state_controller]
+            )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_load_joint_state_controller,
+                on_exit=[action_load_diff_drive_controller]
+            )
+        )
     ])
