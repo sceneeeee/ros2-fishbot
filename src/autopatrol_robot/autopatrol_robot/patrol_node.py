4f8323fae78e377e8b2b14d3bc059476df0cb45d
sceneeeee <peterufo060419@gmail.com>
2026-03-27 20:38:02 +0800

    add autopatrol_robot

---
+++ src/autopatrol_robot/autopatrol_robot/patrol_node.py
@@ -0,0 +1,99 @@
+from geometry_msgs.msg import PoseStamped,Pose
+from nav2_simple_commander.robot_navigator import BasicNavigator,TaskResult
+import rclpy
+from rclpy.node import Node
+import rclpy.time
+from tf2_ros import TransformListener, Buffer # TF变换监听器
+from geometry_msgs.msg import TransformStamped # 消息接口
+from tf_transformations import euler_from_quaternion,quaternion_from_euler # 四元数转欧拉角, 欧拉角转四元数
+from rclpy.duration import Duration
+import math # 角度转弧度
+
+class PatrolNode(BasicNavigator):
+    def __init__(self, node_name="patrol_node"):
+        super().__init__(node_name)
+        self.declare_parameter('initial_point', [0.0, 0.0, 0.0])
+        self.declare_parameter('patrol_points', [
+            0.0, 0.0, 0.0, 
+            1.0, 1.0, 1.57
+            ])
+        self.initial_point = self.get_parameter('initial_point').value
+        self.patrol_points = self.get_parameter('patrol_points').value
+        self.buffer_ = Buffer() # 创建TF变换缓冲区
+        self.listener_ = TransformListener(self.buffer_, self) # 创建TF变换监听器
+
+    def get_pose_by_xyyaw(self, x, y, yaw):
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.pose.position.x = x
+        pose.pose.position.y = y
+        quat = quaternion_from_euler(0, 0, yaw)
+        pose.pose.orientation.x = quat[0]
+        pose.pose.orientation.y = quat[1]
+        pose.pose.orientation.z = quat[2]
+        pose.pose.orientation.w = quat[3]
+        return pose
+        
+    def init_robot_pose(self):
+        self.initial_point_ = self.get_parameter('initial_point').value
+        init_pose = self.get_pose_by_xyyaw(self.initial_point_[0], 
+                                           self.initial_point_[1], 
+                                           self.initial_point_[2])
+        self.setInitialPose(init_pose)
+        self.waitUntilNav2Active()
+        
+    def get_target_point(self):
+        points = []
+        self.target_points_ = self.get_parameter('patrol_points').value
+        for index in range(int(len(self.target_points_)/3)):
+            x = self.target_points_[index*3]
+            y = self.target_points_[index*3+1]
+            yaw = self.target_points_[index*3+2]
+            points.append((x, y, yaw))
+            self.get_logger().info(f"Patrol point {index}: x={x}, y={y}, yaw={yaw}")
+
+        return points
+
+    def nav_to_pose(self, target_pose):
+        self.goToPose(target_pose)
+        while not self.isTaskComplete():
+            feedback = self.getFeedback()
+            self.get_logger().info(f'Navigating to goal: {feedback.distance_remaining:.2f} meters remaining')
+        result = self.getResult()
+        self.get_logger().info(f'Navigation result: {result}')
+
+
+
+    def get_current_pose(self):
+        while rclpy.ok():
+            try:
+                result = self.buffer_.lookup_transform("map",
+                                                        "base_footprint", 
+                                                    rclpy.time.Time(seconds=0),
+                                                    Duration(seconds=1.0)
+                                                    ) # 获取坐标变换
+                
+                transform = result.transform
+                self.get_logger().info(f"translation {transform.translation}")
+                return transform
+            
+            except Exception as e:
+                self.get_logger().error(f"Failed to get transform: {str(e)}")
+
+
+
+    
+    
+def main():
+    rclpy.init()
+    patrol = PatrolNode()
+    patrol.init_robot_pose()
+
+    while rclpy.ok():
+        patrol_points = patrol.get_target_point()
+        for point in patrol_points:
+            x,y,yaw = point[0], point[1], point[2]
+            target_pose = patrol.get_pose_by_xyyaw(x, y, yaw)
+            patrol.nav_to_pose(target_pose)
+
+    rclpy.shutdown()
